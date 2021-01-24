4f7ce1ad4cac7e4ecd90f9b1f8532e7da9904bb5
алина человечкова <alinasmart19@mail.ru>
2021-01-24 19:47:07 +0600

    Добавьте файлы проекта.

---
+++ Homework13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List<int>();
+            var rand = new Random();
+            int listSize = 20;
+
+            for (int i = 0; i < listSize; i++)
+            {
+                list.Add(rand.Next(0, 100));
+            }
+
+            int sum = 0;
+            for (int i = 2; i < listSize; i = i + 2)
+            {
+                sum += list[i];
+            }
+
+            Console.WriteLine(sum);
+
+            int maxValueInList = list.Max();
+            int secondMaxValueInList = 0;
+
+            for (int i = 0; i < listSize; i++)
+            {
+                if (list[i] > secondMaxValueInList && list[i] < maxValueInList)
+                {
+                    secondMaxValueInList = list[i];
+                }
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine($"secondMaxValueInList: {secondMaxValueInList}, item: {list.IndexOf(secondMaxValueInList)}    sum: {sum}");
+
+
+        }
+    }
+}
